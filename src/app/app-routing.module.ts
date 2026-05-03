3b5690cba6652bad42fd7de3dc1007c6e94b8268
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-03 16:14:07 -0500

    feat(routing): configure lazy loading for landing and auth modules
    - Add landing module route at root path
    - Add auth module route at /auth path
    - Add wildcard redirect to root

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/landing/landing.module').then(m => m.LandingModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
