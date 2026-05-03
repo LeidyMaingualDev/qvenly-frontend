212edd6b18ace803362f70708a6bdfe23dccaca6
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-03 16:29:55 -0500

    feat(auth): add auth module
    - Import ReactiveFormsModule for form handling
    - Declare login, register and forgot-password components

---
+++ src/app/features/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
